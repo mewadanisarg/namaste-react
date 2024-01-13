e651e6a29792aae677675ce606f6ed7dc69e1e11
Nisarg Mewada <nisargmewada@gmail.com>
2024-01-13 18:46:57 +0100

    Update Body Component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,28 @@
-import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockData";
+import { useState } from "react";
 
 export default Body = () => {
-  console.log(restaurants);
+  const [restaurantsData, setRestaurantsData] = useState(restaurants);
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("clicked");
+            const filteredRestaurants = restaurantsData.filter(
+              (res) => res.avgRating > 4
+            );
+            setRestaurantsData(filteredRestaurants);
+            console.log(filteredRestaurants);
           }}
         >
           Top Rate Restaurants
         </button>
       </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => {
+        {restaurantsData.map((restaurant) => {
           return <RestaurantCard key={restaurant.id} resData={restaurant} />;
         })}
       </div>
