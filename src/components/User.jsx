c82cca54e4afee6e5e07c6f3b09563d0a5fd24c5
Nisarg Mewada <nisargmewada@gmail.com>
2024-01-29 18:42:46 +0100

    Create User.jsx

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div>
+      <h3>Name: Nirmaan</h3>
+      <h3>Location : Berlin</h3>
+      <h4>Contact@ nm@nm.com</h4>
+    </div>
+  );
+};
+
+export default User;
